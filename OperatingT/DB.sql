8ce3efaf8c0314ebc05af2dc9cd782a92852aff1
SchroDr <1670057209@qq.com>
2019-04-29 19:45:10 +0800

    数据库定义与使用

---
+++ OperatingT/DB.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE operatings;
+CREATE USER 'rua'@'localhost' IDENTIFIED BY 'myfile';
+GRANT ALL ON operatings.* TO 'rua'@'localhost';
--- OperatingT/OperatingT/settings.py
+++ OperatingT/OperatingT/settings.py
@@ -74,14 +74,24 @@ WSGI_APPLICATION = 'OperatingT.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'operatings',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'rua',
+        'PASSWORD': 'myfile',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
